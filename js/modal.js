257844fb2fee8da9dc859c2765c531f1e2835661
Alesya Volota <foxavolt@gmail.com>
2025-06-15 17:41:47 +0200

    modal done

--- js/modal.js
+++ js/modal.js
@@ -3,6 +3,11 @@ const modalBtnOpen = document.querySelector('.modal-btn-open');
 const modalBtnClose = document.querySelector('.modal-btn-close');
 
 const toggleModal = () => modal.classList.toggle('is-hidden');
+const disableScrollModal = () =>
+  document.body.classList.toggle('is-scroll-disabled');
 
 modalBtnOpen.addEventListener('click', toggleModal);
 modalBtnClose.addEventListener('click', toggleModal);
+
+modalBtnOpen.addEventListener('click', disableScrollModal);
+modalBtnClose.addEventListener('click', disableScrollModal);
